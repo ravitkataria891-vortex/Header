cd1087e5ca1d14e9681e0b811cd5634525bfafb0
ravitkataria891-vortex <ravitkataria891@gmail.com>
2026-04-08 17:20:48 +0530

    commit

--- features.c
+++ features.c
@@ -5,3 +5,21 @@
 
 #include "features.h"
 #include "globals.h"
+
+void schemes(void) {
+    FILE *fp = fopen("schemes.txt", "r");
+
+    if (!fp) { 
+        printf("Error: Cannot open schemes.txt!\n"); 
+        return; 
+    }
+    printf("\nGovernment Schemes and Yojanas\n");
+    
+    char line[Maxline];
+    while (fgets(line, Maxline, fp)) {
+        printf("%s", line);
+    }
+    printf("\n");
+    fclose(fp);
+    press_enter();
+}
