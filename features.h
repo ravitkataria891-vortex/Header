0257bd20b545ec34864d54ad3f2c12d8e24935b5
Vasu-Sagwal <Sagwalharsh9@gmail.com>
2026-04-18 15:46:40 +0530

    Add new functions and modify existing declarations

--- features.h
+++ features.h
@@ -1,15 +1,20 @@
 #ifndef Features
 #define Features
 
-void LoadSoilData(void);
 void StateList(void);
 void Recommend(void);
-
-void LoadCost(void);
+void Schemes(void);
 void Profit(void);
 
-void Schemes(void);
+void trim(char *s);
+void flush_input(void);
+void waitforenter(void);
+int str_icmp(const char *a, const char *b);
+void showStates(void);
+void ShowCrops(void);
+int select_state(void);
+int selectcity(int state_idx);
+int selectcrop(void);
+void crop_report(int idx);
 
 #endif
-
-
