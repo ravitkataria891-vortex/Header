cd2b2f8724af60fde8e5078b249d37cc1d52b621
Imdadul Hoque Laskar <2024tpr0014@iitjammu.ac.in>
2026-04-18 15:36:15 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,57 +1,52 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#include"globals.h"
-#include"structs.h"
-#include"features.h"
-#include"sorting.h"
+#include "features.h"
+#include "globals.h"
+#include "sorting.h"
+#include "structs.h"
 
-int MainMenu(void){
-    int choice;
-    printf("\nMain Menu\n");
-    printf("Crop Recommendation\n");
-    printf("Government Schemes\n");
-    printf("Profit Calculation\n");
-    printf("Exit\n");
-    printf("Enter your choice: ");
-    int ret=scanf("%d",&choice);
-    if(ret==EOF)return 0;
-    if(ret!=1)return 0;
-    return choice;
+int MainMenu(void) {
+  int choice;
+  printf("\nMain Menu\n");
+  printf("1. Crop Recommendation\n");
+  printf("2. Government Schemes\n");
+  printf("3. Profit Calculation\n");
+  printf("0. Exit\n");
+  printf("Enter your choice: ");
+  int ret = scanf("%d", &choice);
+  if (ret == EOF)
+    return 0;
+  if (ret != 1) {
+    flush_input();
+    printf("Invalid input\n");
+    return -1;
+  }
+  flush_input();
+  return choice;
 }
 
-int main(void){
-
-
-StateList();
-
-
-SortedPrice(Costdata,Costcount);
-while(1){
-int choice=MainMenu();
-switch(choice){
+int main(void) {
+  initialise_table_null();
+  LoadSoilData();
+  LoadCost();
+  StateList();
+  while (1) {
+    int choice = MainMenu();
+    switch (choice) {
     case 1:
-        Recommend();
-        break;
+      Recommend();
+      break;
     case 2:
-        Schemes();
-        break;
+      Schemes();
+      break;
     case 3:
-        Profit();
-        break;
+      Profit();
+      break;
     case 0:
-        return 0;
+      return 0;
     default:
-        printf("Invalid choice\n");
+      printf("Invalid choice\n");
+    }
+  }
 }
-    
-}
-
-}
-
-
-
-
-
-
-
