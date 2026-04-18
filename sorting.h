2250b83ec1c2c342072d98d8ebe894db552321e0
Vasu-Sagwal <Sagwalharsh9@gmail.com>
2026-04-18 15:42:41 +0530

    Add hash functions and soil/cost lookup declarations

--- sorting.h
+++ sorting.h
@@ -1,9 +1,11 @@
 #ifndef sorting
 #define sorting
 
-unsigned int hash(char *name);
-void initialise_table_null(void);
 void LoadSoilData(void);
 void LoadCost(void);
+unsigned int hash(char *name);
+void initialise_table_null(void);
+int hash_lookup_soil(char *city);
+int hash_lookup_cost(char *crop);
 
 #endif
